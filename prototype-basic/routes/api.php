59298c4cb362fded7bf30f40268fb52241f17526
BkrAyoub <92027292+bkrayoub@users.noreply.github.com>
2022-11-29 10:57:42 +0100

    gg

--- prototype-basic/routes/api.php
+++ prototype-basic/routes/api.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 Route::post('insert', [myController::class , 'insert']);
 
 Route::get('select', [myController::class , 'select']);
